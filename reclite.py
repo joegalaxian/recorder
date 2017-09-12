c3eb3db6cccff1bebf1fac1f581f0dacd6623efb
joegalaxian <joegalaxian@users.noreply.github.com>
2017-09-12 15:38:59 +1000

    Lite version

---
+++ reclite.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import requests
+
+def record(stream_url):
+	"""Records MP3 from parametered URL stream to 'output.mp3'"""
+	r = requests.get(stream_url, stream=True)
+	with open('output.mp3', 'wb') as fd:
+		for chunk in r.iter_content(1024):
+			fd.write(chunk)
+
+if __name__ == '__main__':
+	try: record(sys.argv[1])
+	except: print 'ERROR. Usage: python reclite.py <stream_url>'
