84278c2ce803247e435a21986ce19da82fd4615a
joegalaxian <joegalaxian@users.noreply.github.com>
2017-09-12 15:43:26 +1000

    Lite version - fixed indentation

--- reclite.py
+++ reclite.py
@@ -5,12 +5,14 @@ import sys
 import requests
 
 def record(stream_url):
-	"""Records MP3 from parametered URL stream to 'output.mp3'"""
-	r = requests.get(stream_url, stream=True)
-	with open('output.mp3', 'wb') as fd:
-		for chunk in r.iter_content(1024):
-			fd.write(chunk)
+    """Records MP3 from parametered URL stream to 'output.mp3'"""
+    r = requests.get(stream_url, stream=True)
+    with open('output.mp3', 'wb') as fd:
+        for chunk in r.iter_content(1024):
+            fd.write(chunk)
 
 if __name__ == '__main__':
-	try: record(sys.argv[1])
-	except: print 'ERROR. Usage: python reclite.py <stream_url>'
+    try:
+        record(sys.argv[1])
+    except:
+        print 'ERROR. Usage: python reclite.py <stream_url>'
